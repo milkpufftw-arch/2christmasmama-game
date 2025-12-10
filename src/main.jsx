216f2e31d38b5009313d04da0ce88f05c283e07a
milkpufftw-arch <milkpuff.tw@gmail.com>
2025-12-10 12:35:29 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+// 注意：這裡不要有 import './index.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-
 )
